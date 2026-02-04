987e590ec56c50a4887504dfa2f272db52562d10
Bruno Silvestre <bruno.silvestre@gmail.com>
2026-02-04 19:20:00 -0300

    Example of FreeBSD generic netlink command

--- README.md
+++ README.md
@@ -27,3 +27,37 @@ How to register a new generic netlink family on FreeBSD.
 `$ cd example-01/kernel`
 
 `$ doas make unload`
+
+---
+
+## Example 02
+
+How to register a command, with no attributes (parameters), in the generic netlink family and call it from the userspace.
+
+### Build the FreeBSD kernel module
+
+`$ cd example-02/kernel`
+
+`$ make`
+
+### Load the module
+
+`$ cd example-02/kernel`
+
+`$ doas make load`
+
+### Build and run the client
+
+`$ cd example-02/user`
+
+`$ make`
+
+`$ ./nl_echo_client`
+
+### Unload the module
+
+`$ cd example-02/kernel`
+
+`$ doas make unload`
+
+
---
+++ example-02/user/nl_echo_client.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <netlink/netlink_snl_generic.h>
+
+#define FAMILY_NAME "nl_echo"
+
+/*
+ * Commands  (TODO: do not copy, share it with kernel)
+ */
+enum {
+	ECHO_CMD_UNSPEC = 0,
+	ECHO_CMD_REPLY  = 1,
+	__ECHO_CMD_MAX,
+};
+#define ECHO_CMD_MAX (__ECHO_CMD_MAX - 1)
+
+
+int main()
+{
+	struct snl_state ss;
+	struct snl_writer nw;
+	struct nlmsghdr *hdr;
+
+	if (!snl_init(&ss, NETLINK_GENERIC)) {
+		printf("[NL_ECHO] Init error.\n");
+		return (1);
+	}
+
+	uint16_t family_id = snl_get_genl_family(&ss, FAMILY_NAME);
+	if (family_id == 0) {
+		printf("[NL_ECHO] Family '%s' not found.\n", FAMILY_NAME);
+		snl_free(&ss);
+		return (1);
+	}
+
+	printf("[NL_ECHO] Family ID: %u\n", family_id);
+
+	/* Prepare the request
+	 *
+	 * Note: snl_create_genl_msg_request does not set the family version in
+	 * the generic netlink header, i.e., version = 0. If the kernel checks the
+	 * family version for compatibility, this could be a problem.
+	 */
+	snl_init_writer(&ss, &nw);
+	hdr = snl_create_genl_msg_request(&nw, family_id, ECHO_CMD_REPLY);
+	if (hdr == NULL) {
+		printf("[NL_ECHO] Create request error\n");
+		snl_free(&ss);
+		return (1);
+	}
+
+	/* Send the message */
+	if ((hdr = snl_finalize_msg(&nw)) == NULL || !snl_send_message(&ss, hdr)) {
+		printf("[NL_ECHO] Send message error\n");
+		snl_free(&ss);
+		return (1);
+	}
+
+	/* Receive the reply from the sequence number */
+	hdr = snl_read_reply(&ss, hdr->nlmsg_seq);
+	if (hdr == NULL || hdr->nlmsg_type == NLMSG_ERROR) {
+		printf("[NL_ECHO] Reply error\n");
+		snl_free(&ss);
+		return (1);
+	}
+
+	printf("[NL_ECHO] Reply received\n");
+
+	snl_free(&ss);
+	return (0);
+}
